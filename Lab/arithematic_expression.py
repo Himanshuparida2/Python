00bdcde211a21b464729c7012bac37fe0a204886
Himanshu Sekhar Parida <118821114+Himanshuparida2@users.noreply.github.com>
2024-09-13 01:48:55 +0530

    Add files via upload

---
+++ Lab/arithematic_expression.py
@@ -0,0 +1,10 @@
+x=2
+y=4
+print("x=2 y=4")
+print("addsion x+y=",x+y);
+print("substraction x-y=",x-y);
+print("true division x/y=",x/y);
+print("Floor division x//y=",x//y);
+print("Exponential x**y=",x**y);
+print("multiplication x*y=",x*y);
+print("Reminder x%y=",x%y);
---
+++ Lab/firstcode.py
@@ -0,0 +1,4 @@
+print("This is the first python program in script mode");
+x = int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+print("Your Sum is ",x+y);
