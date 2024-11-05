92ba7952ff7e39365cc6db0b63e489433ececd2e
Himanshu Sekhar Parida <118821114+Himanshuparida2@users.noreply.github.com>
2024-11-05 21:40:50 +0530

    Add files via upload

---
+++ Lab/BarGraph.py
@@ -0,0 +1,5 @@
+#Creating Bar Chart
+bar=2,5,3,6,7
+print("index\tvalue")
+for index,value in enumerate(bar):
+    print(index,"\t",value,value*'*')
---
+++ Lab/tuple.py
@@ -0,0 +1,5 @@
+t= 'a','b','c'
+colors='red','blue','white','black'
+print('Index',"\t",'Colors')
+for x in enumerate(colors):
+    print(x[0],"\t",x[1])
